2c46d2f9c94bf24fa2408ad3715f75252e4d4430
Thuc VX <vxthuc@yahoo.com>
2017-06-01 12:30:07 +0700

    Update dependency and ignore file

--- app/tests/test_options.py
+++ app/tests/test_options.py
@@ -8,16 +8,9 @@ class TestCommandLineParameters(unittest.TestCase):
     def setUp(self):
         self.options = Options()
 
-    def test_defaults_options_are_set(self):
-        opts = self.options.parse()
-        self.assertEquals(opts.example, 'example-value')
-
     def test_options_example_is_set(self):
-        opts = self.options.parse(['-x', 'foobar'])
-        self.assertEquals(opts.example, 'foobar')
-
-        opts = self.options.parse(['--example', 'not-a-foobar'])
-        self.assertEquals(opts.example, 'not-a-foobar')
+        opts = self.options.parse(['-c', 'checking_interval'])
+        self.assertEquals(opts.example, '2')
 
 
 if __name__ == '__main__':
